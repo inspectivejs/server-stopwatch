1a99a5f8ee2ae14287c6b3e713efd3d652c758e4
AaronAddison <abartaddison12@gmail.com>
2018-10-15 18:35:45 -0400

    create reducer file and export reducers for store implementation

--- app/reducers/stopWatchReducer.js
+++ app/reducers/stopWatchReducer.js
@@ -1,5 +1,36 @@
 import * as types from '../actions/actionTypes';
 
 const initialState = {
-  
+  filePath: '',
+  URL: 'http://localhost:3000',
+  method: 'GET',
+  headers: {
+    Accept: '*/*',
+    'User Agent': 'HTTP Wizard'
+  },
 }
+
+const stopWatchReducer = (state=initialState, action) => {
+  switch(action.type){
+    case types.SET_SERVER_PATH:
+      return {
+        ...state,
+        filePath: action.payload,
+      };
+    case types.SET_URL:
+      return {
+        ...state,
+        URL: action.payload,
+      };
+    case types.SET_METHOD:
+      console.log(action.payload);
+      return {
+        ...state,
+        method: action.payload,
+      }
+    default:
+      return state;
+  }
+}
+
+export default stopWatchReducer;
